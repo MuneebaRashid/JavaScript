dbf7fbce6f0bdee9dca56b5b04b9672ad89c6862
MuneebaRashid <muneebarashidhere@gmail.com>
2023-04-06 16:34:50 +0500

    Initial commit

---
+++ Daily-Practice/Class-1/app.js
@@ -0,0 +1,37 @@
+//  Getting started with JS
+alert("Hello World");
+//variables // var // let
+let name = "Muneeba";
+ alert(name);
+ //variable rules:
+ // 1. Legal or Illegal Names
+    // In Legal names, camelCAse id used for variable names. 
+    //No space. not start with number
+    let fullName = "Muneeba Rashid";
+    alert(fullName);
+    // snake_case uses underscore in variable names
+    // example, let _fullName_ = "Muneeba Rashid";
+    // $ sign can also be used in legal variables.
+    // example, let $fullName = "Muneeba Rashid";
+ 
+    // 2. For illegal, spaces are used 
+    // spaces and special characters apart from $ and _ are illegal cases
+    // example let my#name is illegal.
+    // reserved words example, alert
+    
+    //Data Types
+    // 1. String:
+       //written in single ' ' or double " " inverted commas or quotes
+       //but use any one in all over project 
+       //any letter, word, or sentence 
+       //special characters are also strings
+       // e.g: "Name" , "555" , "Any Thing" , "muneebarashidhere@gmail.com"
+    // 2. Number:
+       // without inverted commas
+       // if written with inverted comma, it will become string
+       let myDateOfBirth = 14;
+       //decimal and negative values also included
+    //3. Boolean:
+       //2 values either true or either false
+       let areYouAHuman = true;
+
